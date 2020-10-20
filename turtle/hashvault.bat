bf7e7e0c511113008e945917c5aa0d10b458098e
meganano202 <45272190+meganano202@users.noreply.github.com>
2020-10-20 19:22:46 +0200

    Add files via upload

---
+++ turtle/hashvault.bat
@@ -0,0 +1,1 @@
+xmrig --url pool.hashvault.pro:80 --user TRTLuzpvkAnBnUpRewNvjiN12kvfUSLDSKixaujp5KJ87EVqfTgknyYeJwQTGfvTrCJLjnRzsX7vXQUHzhWnMGfJbgSGhWmdom3 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
